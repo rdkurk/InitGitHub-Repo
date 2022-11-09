e9ba4857b11058f187ba8eab4ac8227f70a0d7a4
Richard Kurk <rdkurk@msn.com>
2022-11-09 11:50:11 -0800

    Initial Commit

---
+++ Test GitHub App/Test_GitHub_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Test_GitHub_AppApp.swift
+//  Test GitHub App
+//
+//  Created by Richard Kurk on 11/9/22.
+//
+
+import SwiftUI
+
+@main
+struct Test_GitHub_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
